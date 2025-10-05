c9635dc466130f6591212dc90f1ce2250715e9fb
gustamara <gustacamra.dev@hotmail.com>
2025-10-05 05:34:46 -0300

    streamline graph initialization in Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,16 +2,9 @@ package app;
 
 import Controllers.DatasetController;
 import Controllers.GraphController;
-import infraestructure.DatasetReader;
 import model.Graph;
-import model.RawDataNode;
-
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.io.FileWriter;
-import java.util.Arrays;
+import java.util.Map;
 
 public class Main {
     public static final String DATASET = "./src/data/maildir";
@@ -20,8 +13,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         DatasetController datasetController = new DatasetController(DATASET, DATASET_REDUCED);
         datasetController.runRoutine();
-        GraphController g = new GraphController();
-        g.runGraph();
+        Map<String, Map<String, Integer>> adj = datasetController.getAdjacencyRelationShipp();
+        GraphController gc = new GraphController();
+         Graph g = gc.setAdjByHash(adj);
+
+        gc.runGraph(g);
     }
+
 
 }
