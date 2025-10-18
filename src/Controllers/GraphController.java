7ffa3e09af2a6bfd87d059d4890118dde5d1421b
gustamara <gustacamra.dev@hotmail.com>
2025-10-17 21:15:38 -0300

    refactor runGraph and setAdjByHash methods to improve readability and efficiency

--- src/Controllers/GraphController.java
+++ src/Controllers/GraphController.java
@@ -4,53 +4,69 @@ import model.Graph;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 
 public class GraphController {
     public void runGraph(Graph g) {
-        System.out.println("Lista de adjacencias: \n");
-        g.printAdjacency();
+        System.out.println("Numero de vertices: " + g.getMax());
+        System.out.println("Numero de arestas: " + g.getAdjacencyCount());
+
+        int topSender = g.getIndex("sally.beck@enron.com");
+        int topReceaver = g.getIndex("john.lavorato@enron.com");
 
         System.out.print("Grau de Saida: ");
-        ArrayList<Integer> listOfOutputAdjacency =  new ArrayList<>();
-        System.out.print(g.getLabelById(87));
-        System.out.println(" : " + g.outputVertexGrade(87, listOfOutputAdjacency));
+        ArrayList<Integer> listOfOutputAdjacency = new ArrayList<>();
+        System.out.print(g.getLabelById(topSender));
+        System.out.println(" : " + g.outputVertexGrade(topSender, listOfOutputAdjacency));
         g.topOutputGrade();
 
         System.out.print("\nGrau de entrada: ");
-        ArrayList<Integer> listOfInputAdjacency =  new ArrayList<>();
-        System.out.print(g.getLabelById(90));
-        System.out.println(" : " + g.inputVertexGrade(90, listOfInputAdjacency));
+        ArrayList<Integer> listOfInputAdjacency = new ArrayList<>();
+        System.out.print(g.getLabelById(topReceaver));
+        System.out.println(" : " + g.inputVertexGrade(topReceaver, listOfInputAdjacency));
         g.topInputGrade();
 
-        int fromId = 88; // 88, 10
-        int toId = 10; // 104, 50
-        System.out.println("\nNumero de vertices: " + g.getMax());
-        System.out.println("Numero de arestas: " + g.getAdjacencyCount());
-        System.out.println("Busca em largura: " + g.breadthFirstSearch(fromId, toId));
-        System.out.println("Busca em profundidade: " + g.deepFirsSearch(fromId, toId));
-        System.out.println("Lista de alcançabilidade("+  g.getLabelById(fromId)+"): " + g.reach(fromId,3));
+        System.out.println("Busca em largura: " + g.breadthFirstSearch(topSender, topReceaver));
+        System.out.println("Busca em profundidade: " + g.deepFirsSearch(topSender, topReceaver));
+        System.out.println("Lista de alcançabilidade(" + g.getLabelById(topReceaver) + "): " + g.reach(topReceaver, 5));
+        
         ArrayList<Integer> path = new ArrayList<>();
-        System.out.println("Custo Dijkstra: " + g.dijkstra(fromId, toId, path));
+        System.out.println("Custo Dijkstra: " + g.dijkstra(topSender, topReceaver, path));
         System.out.println("Caminho Dijkstra: " + path);
-
     }
 
     public Graph setAdjByHash(Map<String, Map<String, Integer>> map){
-        Map<String, Integer> key;
-        String[] labels, toList;
-        int frequency;
-        labels = map.keySet().toArray(new String[0]);
+        Set<String> allEmails = new HashSet<>();
+        for(String from: map.keySet()) {
+            allEmails.add(from);
+            Map<String, Integer> destinations = map.get(from);
+            allEmails.addAll(destinations.keySet()); 
+        }
+        
+        String[] labels = allEmails.toArray(new String[0]);
+        System.out.println("Criando grafo com " + labels.length + " vértices únicos");
+        System.out.println("Processando " + map.keySet().size() + " remetentes ativos");
+        
         Graph g = new Graph(labels);
-        for(String from: labels) {
-
-            key = map.get(from);
-            toList = key.keySet().toArray(new String[0]);
-
-            for (String s : toList) {
-                frequency = key.get(s);
-                g.createAdjacency(g.getIndex(from), g.getIndex(s), frequency);
+        int totalAdjacenciesAdded = 0;
+        
+        for(String from: map.keySet()) {
+            Map<String, Integer> destinations = map.get(from);
+            
+            for (Map.Entry<String, Integer> entry : destinations.entrySet()) {
+                String to = entry.getKey();
+                int frequency = entry.getValue();
+                
+                int fromIndex = g.getIndex(from);
+                int toIndex = g.getIndex(to);
+                
+                g.createAdjacency(fromIndex, toIndex, frequency);
+                totalAdjacenciesAdded++;
             }
         }
+        
+        System.out.println("Total de " + totalAdjacenciesAdded + " arestas adicionadas com sucesso");
         return g;
     }
 }
